128c699dec486b666fa9ea57bfef0401249a1a2a
Zeokat <zeokat@gmail.com>
2013-12-28 22:07:37 +0100

    Primera actualizacion

---
+++ Twitter OAuth (Crear post) - Vozidea.com/Form1.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace Twitter_OAuth__Crear_post____Vozidea.com
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox_consumerk = new System.Windows.Forms.TextBox();
+            this.textBox_consumers = new System.Windows.Forms.TextBox();
+            this.textBox_atoken = new System.Windows.Forms.TextBox();
+            this.textBox_atokensecret = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox_post = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Consumer Key";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 35);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(88, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Consumer Secret";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 61);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(76, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Access Token";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 87);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(110, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Access Token Secret";
+            // 
+            // textBox_consumerk
+            // 
+            this.textBox_consumerk.Location = new System.Drawing.Point(93, 6);
+            this.textBox_consumerk.Name = "textBox_consumerk";
+            this.textBox_consumerk.Size = new System.Drawing.Size(438, 20);
+            this.textBox_consumerk.TabIndex = 4;
+            // 
+            // textBox_consumers
+            // 
+            this.textBox_consumers.Location = new System.Drawing.Point(106, 32);
+            this.textBox_consumers.Name = "textBox_consumers";
+            this.textBox_consumers.Size = new System.Drawing.Size(425, 20);
+            this.textBox_consumers.TabIndex = 5;
+            // 
+            // textBox_atoken
+            // 
+            this.textBox_atoken.Location = new System.Drawing.Point(94, 58);
+            this.textBox_atoken.Name = "textBox_atoken";
+            this.textBox_atoken.Size = new System.Drawing.Size(437, 20);
+            this.textBox_atoken.TabIndex = 6;
+            // 
+            // textBox_atokensecret
+            // 
+            this.textBox_atokensecret.Location = new System.Drawing.Point(128, 84);
+            this.textBox_atokensecret.Name = "textBox_atokensecret";
+            this.textBox_atokensecret.Size = new System.Drawing.Size(403, 20);
+            this.textBox_atokensecret.TabIndex = 7;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 112);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(205, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Texto del Tweet (máximo 140 caracteres):";
+            // 
+            // textBox_post
+            // 
+            this.textBox_post.Location = new System.Drawing.Point(15, 128);
+            this.textBox_post.MaxLength = 140;
+            this.textBox_post.Multiline = true;
+            this.textBox_post.Name = "textBox_post";
+            this.textBox_post.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox_post.Size = new System.Drawing.Size(516, 62);
+            this.textBox_post.TabIndex = 9;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(443, 196);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(88, 23);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Enviar Tweet";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(543, 231);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox_post);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox_atokensecret);
+            this.Controls.Add(this.textBox_atoken);
+            this.Controls.Add(this.textBox_consumers);
+            this.Controls.Add(this.textBox_consumerk);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Twitter OAuth (Crear Post) - Vozidea.com";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox_consumerk;
+        private System.Windows.Forms.TextBox textBox_consumers;
+        private System.Windows.Forms.TextBox textBox_atoken;
+        private System.Windows.Forms.TextBox textBox_atokensecret;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox_post;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Twitter OAuth (Crear post) - Vozidea.com/Form1.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+using System.Net;
+using System.IO;
+
+namespace Twitter_OAuth__Crear_post____Vozidea.com
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Llaves de acceso de la aplicación Twitter
+            var oauth_token = textBox_atoken.Text;
+            var oauth_token_secret = textBox_atokensecret.Text;
+            var oauth_consumer_key = textBox_consumerk.Text;
+            var oauth_consumer_secret = textBox_consumers.Text;
+
+            // Caracteristicas de la implementacion OAuth
+            var oauth_version = "1.0";
+            var oauth_signature_method = "HMAC-SHA1";
+
+            // Detalles del mensaje de la API
+            var mensaje = textBox_post.Text;
+            var resource_url = "https://api.twitter.com/1.1/statuses/update.json";
+            var request_type = "POST";
+
+            // Detalles de creación de la cabecera de autentificación
+            // Creamos el oauth_nonce
+            var oauth_nonce = CreateNonce();
+            // Creamos el oauth_timestamp
+            var oauth_timestamp = CreateTimestamp();
+            // Creamos el oauth_signature
+            string oauth_signature = CreateOAuthSignature(oauth_consumer_key, oauth_nonce, oauth_signature_method,
+                oauth_timestamp, oauth_token, oauth_version, mensaje, resource_url, request_type, oauth_consumer_secret, oauth_token_secret);
+
+            // Construimos la cabecera de autentificación combinando todos los valores
+            var headerFormat = "OAuth oauth_consumer_key=\"{0}\", oauth_nonce=\"{1}\", oauth_signature=\"{2}\", " +
+                               "oauth_signature_method=\"{3}\", oauth_timestamp=\"{4}\", " +
+                               "oauth_token=\"{5}\", oauth_version=\"{6}\"";
+
+            var authHeader = string.Format(headerFormat,
+                                    Uri.EscapeDataString(oauth_consumer_key),
+                                    Uri.EscapeDataString(oauth_nonce),
+                                    Uri.EscapeDataString(oauth_signature),
+                                    Uri.EscapeDataString(oauth_signature_method),
+                                    Uri.EscapeDataString(oauth_timestamp),
+                                    Uri.EscapeDataString(oauth_token),
+                                    Uri.EscapeDataString(oauth_version)
+                            );
+
+            // Realizamos la petición HTTP con el método POST
+            var postdata = "status=" + URLEncode(mensaje);
+            ServicePointManager.Expect100Continue = false;
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(resource_url);
+            request.Headers.Add("Authorization", authHeader);
+            request.Method = request_type;
+            request.ContentType = "application/x-www-form-urlencoded;charset=UTF-8";
+            using (Stream stream = request.GetRequestStream())
+            {
+                byte[] content = ASCIIEncoding.ASCII.GetBytes(postdata);
+                stream.Write(content, 0, content.Length);
+            }
+            WebResponse response = request.GetResponse();
+            StreamReader sr = new StreamReader(response.GetResponseStream());
+            string json = sr.ReadToEnd();
+            sr.Close();
+            response.Close();
+
+            textBox_post.Text = json;
+        }
+
+        private string CreateNonce()
+        {
+            var oauth_nonce = CalculateMD5Hash(DateTime.Now.Ticks.ToString());
+            return oauth_nonce;
+        }
+
+        private string CreateTimestamp()
+        {
+            var timeSpan = DateTime.UtcNow - new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+            var oauth_timestamp = Convert.ToInt64(timeSpan.TotalSeconds).ToString();
+            return oauth_timestamp;
+        }
+
+        private string CreateOAuthSignature(string oauth_consumer_key, string oauth_nonce, string oauth_signature_method,
+            string oauth_timestamp, string oauth_token, string oauth_version, string mensaje, string resource_url,
+            string request_type, string oauth_consumer_secret, string oauth_token_secret)
+        {
+            var baseFormat = "oauth_consumer_key={0}&oauth_nonce={1}&oauth_signature_method={2}" +
+                            "&oauth_timestamp={3}&oauth_token={4}&oauth_version={5}&status={6}";
+
+            var baseString = string.Format(baseFormat,
+                                        oauth_consumer_key,
+                                        oauth_nonce,
+                                        oauth_signature_method,
+                                        oauth_timestamp,
+                                        oauth_token,
+                                        oauth_version,
+                                        URLEncode(mensaje)
+                                        );
+            baseString = string.Concat(request_type, "&", Uri.EscapeDataString(resource_url), "&", Uri.EscapeDataString(baseString));
+
+
+            var SHA_Key = string.Concat(Uri.EscapeDataString(oauth_consumer_secret), "&", Uri.EscapeDataString(oauth_token_secret));
+            string oauth_signature;
+            using (HMACSHA1 hasher = new HMACSHA1(ASCIIEncoding.ASCII.GetBytes(SHA_Key)))
+            {
+                oauth_signature = Convert.ToBase64String(hasher.ComputeHash(ASCIIEncoding.ASCII.GetBytes(baseString)));
+            }
+
+            return oauth_signature;
+        }
+
+        public string CalculateMD5Hash(string input)
+        {
+            // Paso 1, calculamos el MD5 a partir del input
+            MD5 md5 = System.Security.Cryptography.MD5.Create();
+            byte[] inputBytes = System.Text.Encoding.ASCII.GetBytes(input);
+            byte[] hash = md5.ComputeHash(inputBytes);
+
+            // Paso 2, convertimos el byte array a una cadena hexadecimal
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < hash.Length; i++)
+            {
+                sb.Append(hash[i].ToString("X2"));
+            }
+            return sb.ToString().ToLower();
+        }
+
+        private static string URLEncode(string s)
+        {
+            var sb = new StringBuilder();
+
+            foreach (byte c in Encoding.UTF8.GetBytes(s))
+            {
+                if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || (c >= '0' && c <= '9') || c == '-' || c == '_' || c == '.' || c == '~')
+                    sb.Append((char)c);
+                else
+                {
+                    sb.AppendFormat("%{0:X2}", c);
+                }
+            }
+            return sb.ToString();
+        }
+    }
+}
